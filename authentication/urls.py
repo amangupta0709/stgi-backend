8be379af2cbcd0899cb67dbeaba24ce66cc9e310
Aman Gupta <guptaaman704@gmail.com>
2022-11-24 13:52:17 +0530

    authentication created

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from knox import views as knox_views
+
+from authentication.views import LoginAPIView, RegisterAPIView
+
+urlpatterns = [
+    path("register/", RegisterAPIView.as_view(), name="register"),
+    path("login/", LoginAPIView.as_view(), name="login"),
+    path("logout/", knox_views.LogoutView.as_view(), name="logout"),
+]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+# from django.contrib.auth.tokens import default_token_generator
+from knox.models import AuthToken
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from authentication.serializers import (
+    LoginSerializer,
+    RegisterSerializer,
+    UserSerializer,
+)
+
+
+class RegisterAPIView(GenericAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        serialized_data = UserSerializer(
+            user, context=self.get_serializer_context()
+        ).data
+        return Response(
+            {"token": AuthToken.objects.create(user)[1]}, status=status.HTTP_201_CREATED
+        )
+
+
+class LoginAPIView(GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        serialized_data = UserSerializer(
+            user, context=self.get_serializer_context()
+        ).data
+        return Response(
+            {"token": AuthToken.objects.create(user)[1]}, status=status.HTTP_200_OK
+        )
